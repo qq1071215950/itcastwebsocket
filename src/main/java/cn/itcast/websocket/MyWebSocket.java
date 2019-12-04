140df6936bf824237218eee68128fbf4096b7a01
lijian <1071215950@qq.com>
2019-12-04 22:07:35 +0800

    Initial commit

---
+++ src/main/java/cn/itcast/websocket/MyWebSocket.java
@@ -0,0 +1,29 @@
+package cn.itcast.websocket;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
+@ServerEndpoint("/websocket/{uid}")
+public class MyWebSocket {
+    @OnOpen
+    public void onOpen(Session session, @PathParam("uid") String uid) throws IOException {
+        // 连接成功
+        session.getBasicRemote().sendText(uid + "，你好，欢迎连接WebSocket！");
+    }
+
+    @OnClose
+    public void onClose() {
+        System.out.println(this + "关闭连接");
+    }
+    @OnMessage
+    public void onMessage(String message, Session session) throws IOException {
+        System.out.println("接收到消息：" + message);
+        session.getBasicRemote().sendText("消息已收到.");
+    }
+    @OnError
+    public void onError(Session session, Throwable error) {
+        System.out.println("发生错误"); error.printStackTrace();
+    }
+}
---
+++ src/main/java/cn/itcast/websocket/spring/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package cn.itcast.websocket.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Autowired
+    private MyHandshakeInterceptor myHandshakeInterceptor;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/ws").setAllowedOrigins("*").addInterceptors(this.myHandshakeInterceptor);
+    }
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
+    }
+}
